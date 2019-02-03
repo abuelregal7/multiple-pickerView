b63b711c839491a51cfb4c5ebcb2b892bf8d00d8
ahmed abu elregal <ahmedabuelregal@192.168.181.130>
2019-02-02 17:09:40 -0800

    14

--- multiple pickerView/ViewController.swift
+++ multiple pickerView/ViewController.swift
@@ -8,9 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
+    
+    var selected : Int!
+    
+    var boysNames : [String] = ["Ahmed","mohameed","Ramy","Yaseen","Adam","Essam","Kamel","Samier"]
+    var girlsNames : [String] = ["Gehan","menna","fatema","Shreen","Nagat","Eman","Rania","Walaa"]
+    
+    @IBOutlet weak var namesOfBoys: UIPickerView!
+    @IBOutlet weak var namesOfGirls: UIPickerView!
+    
     override func viewDidLoad() {
+        
+        namesOfBoys.dataSource = self
+        namesOfBoys.delegate = self
+        namesOfGirls.dataSource = self
+        namesOfGirls.delegate = self
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -18,6 +32,34 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if (selected == 0)
+        {
+            return boysNames.count
+        }
+        else
+        {
+            return girlsNames.count
+        }
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if (pickerView == self.namesOfBoys)
+        {
+            return "\(boysNames[row])"
+        }
+        else if (pickerView == self.namesOfGirls)
+        {
+            return "\(girlsNames[row])"
+        }
+        else {
+            return "0"
+        }
     }
 
 
